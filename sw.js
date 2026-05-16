d453a71d687bf1d8a39a0f54fe64bb2bb361d5ac
danielsmith210409-lang <danielsmith210409@gmail.com>
2026-05-16 22:18:42 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('fetch', function(event) {
-    // This leaves the door open for offline functionality later
+const CACHE_NAME = 'ma-ford-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './sw.js'
+];
+
+// Install and Cache Files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve Files from Cache when Offline or Cold-Starting
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
